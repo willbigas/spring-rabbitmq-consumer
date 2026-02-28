ce335ec87c1cbe6a82ef097e5d13954a5caa118e
William Bigas Mauro <williambmauro@hotmail.com>
2026-02-28 14:32:56 -0300

    Adicao de DLQ no consumer e separacao de classe para as configs de uma fila especifica

--- src/main/java/br/com/willbigas/rabbitmq/config/RabbitConfig.java
+++ src/main/java/br/com/willbigas/rabbitmq/config/RabbitConfig.java
@@ -1,27 +1,32 @@
 package br.com.willbigas.rabbitmq.config;
 
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
 @Configuration
 public class RabbitConfig {
 
-    public static final String QUEUE_NAME = "notification.queue";
+	@Bean
+	public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+		SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+		factory.setConnectionFactory(connectionFactory);
+		factory.setConsumerTagStrategy(queue -> "spring-rabbitmq-consumer-" + queue);
+		return factory;
+	}
 
-    @Bean
-    public Queue notificationQueue() {
-        return new Queue(QUEUE_NAME, true); // durable = true
-    }
-
-    /**
-     * Configura pra deserializar em Json
-     */
-    @Bean
-    public MessageConverter messageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
+	/**
+	 * Configura pra deserializar em Json
+	 */
+	@Bean
+	public MessageConverter messageConverter(ObjectMapper objectMapper) {
+		return new Jackson2JsonMessageConverter(objectMapper);
+	}
 
 }
--- src/main/java/br/com/willbigas/rabbitmq/consumer/NotificationConsumer.java
+++ src/main/java/br/com/willbigas/rabbitmq/consumer/NotificationConsumer.java
@@ -1,6 +1,6 @@
 package br.com.willbigas.rabbitmq.consumer;
 
-import br.com.willbigas.rabbitmq.config.RabbitConfig;
+import br.com.willbigas.rabbitmq.config.NotificationRabbitConfig;
 import br.com.willbigas.rabbitmq.dto.NotificationMessage;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
@@ -8,16 +8,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class NotificationConsumer {
 
-	@RabbitListener(queues = RabbitConfig.QUEUE_NAME)
+    @RabbitListener(queues = NotificationRabbitConfig.QUEUE_NAME)
     public void handleNotification(NotificationMessage message) {
-        System.out.println("Received: " + message);
-
-        // Simulate processing
         try {
+            System.out.println("Received: " + message);
+            // Simulate processing
             Thread.sleep(1000); // 1 second processing time
             System.out.println("Notification sent to: " + message.to());
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+        } catch (Exception e) {
+            // Lançando exceção para que o RabbitMQ envie para a DLQ
+            throw new RuntimeException("Erro ao processar mensagem, enviando para DLQ", e);
         }
+    }
+
+    @RabbitListener(queues = NotificationRabbitConfig.DLQ_NAME)
+    public void handleDlq(NotificationMessage message) {
+        System.err.println("Mensagem encaminhada para DLQ: " + message);
+        // Aqui você pode implementar lógica de alerta, log ou retentativa manual
     }
 }
