89039ccf2829d3998095500c0e60ebc256a4ed38
William Bigas Mauro <williambmauro@hotmail.com>
2026-02-28 15:00:25 -0300

    Adicao de configuração de DLQ para fallback em caso de erro de processamento.

--- src/main/java/br/com/willbigas/rabbitmq/config/RabbitConfig.java
+++ src/main/java/br/com/willbigas/rabbitmq/config/RabbitConfig.java
@@ -1,11 +1,14 @@
 package br.com.willbigas.rabbitmq.config;
 
+import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.retry.RejectAndDontRequeueRecoverer;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.interceptor.RetryOperationsInterceptor;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -14,11 +17,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class RabbitConfig {
 
 	@Bean
-	public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+	public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory, RetryOperationsInterceptor retryInterceptor) {
 		SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
 		factory.setConnectionFactory(connectionFactory);
 		factory.setConsumerTagStrategy(queue -> "spring-rabbitmq-consumer-" + queue);
 		factory.setMessageConverter(messageConverter(new ObjectMapper()));
+		factory.setAdviceChain(retryInterceptor); // Adiciona o interceptor de retry
 		return factory;
 	}
 
@@ -28,6 +32,19 @@ public class RabbitConfig {
 	@Bean
 	public MessageConverter messageConverter(ObjectMapper objectMapper) {
 		return new Jackson2JsonMessageConverter(objectMapper);
+	}
+
+	/**
+	 * Configura Retry no consumo usando Spring Retry
+	 * @return
+	 */
+	@Bean
+	public RetryOperationsInterceptor retryInterceptor() {
+	    return RetryInterceptorBuilder.stateless()
+	        .maxAttempts(3)
+	        .backOffOptions(1000, 2.0, 10000) // delay inicial, multiplicador, delay máximo (opcional)
+	        .recoverer(new RejectAndDontRequeueRecoverer()) // Garante rejeição para DLQ
+	        .build();
 	}
 
 }
--- src/main/java/br/com/willbigas/rabbitmq/consumer/NotificationConsumer.java
+++ src/main/java/br/com/willbigas/rabbitmq/consumer/NotificationConsumer.java
@@ -15,6 +15,7 @@ public class NotificationConsumer {
             // Simulate processing
             Thread.sleep(1000); // 1 second processing time
             System.out.println("Notification sent to: " + message.to());
+            throw new RuntimeException("Erro ao processar mensagem");
         } catch (Exception e) {
             // Lançando exceção para que o RabbitMQ envie para a DLQ
             throw new RuntimeException("Erro ao processar mensagem, enviando para DLQ", e);
